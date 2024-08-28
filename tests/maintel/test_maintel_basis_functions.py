f0bad9f0821609572234bfe13ac662335e5897df
Keith Bechtol <kbechtol@lsst.org>
2024-08-28 16:50:28 +0000

    Add unit tests for basis functions and detailers

---
+++ tests/maintel/test_maintel_basis_functions.py
@@ -0,0 +1,31 @@
+# This file is part of ts_fbs_utils.
+#
+# Developed for the Vera Rubin Observatory Telescope and Site System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from lsst.ts.fbs.utils.maintel.basis_functions import get_basis_functions_field_survey
+
+
+def test_get_basis_functions_field_survey() -> None:
+    basis_functions = get_basis_functions_field_survey(
+        nside=32,
+        wind_speed_maximum=13.0,
+    )
+
+    assert len(basis_functions) == 7
