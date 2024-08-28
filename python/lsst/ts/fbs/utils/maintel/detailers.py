0e51c9ec5849a229ab9f44b2da40229d9a5e637f
Keith Bechtol <kbechtol@lsst.org>
2024-08-28 15:54:47 +0000

    Add detailers

---
+++ python/lsst/ts/fbs/utils/maintel/detailers.py
@@ -0,0 +1,65 @@
+# This file is part of ts_fbs_utils.
+#
+# Developed for the Vera Rubin Observatory Telescope and Site System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# This file is part of ts_fbs_utils.
+#
+# Developed for the Vera Rubin Observatory Telescope and Site System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__all__ = [
+    "get_detailers_field_survey",
+]
+
+from rubin_scheduler.scheduler import detailers
+
+def get_detailers_field_survey() -> list[detailers.BaseDetailer]:
+    """Get the detailers for a field survey.
+
+    Parameters
+    ----------
+    
+
+    Returns
+    -------
+    `list` of `detailers.BaseDetailer`
+    """
+    
+    detailer_list = [
+        detailers.CameraSmallRotPerObservationListDetailer(per_visit_rot=0.5),
+        detailers.DitherDetailer(max_dither=0.2, per_night=False),
+    ]
+    return detailer_list
