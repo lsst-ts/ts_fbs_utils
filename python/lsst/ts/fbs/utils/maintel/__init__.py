69c714e5935cb46cbce9cb899cce6d499523c30e
Lynne Jones <ljones.uw@gmail.com>
2025-10-19 15:19:22 -0700

    Minimize init and rename sv version of ddf_presched

--- python/lsst/ts/fbs/utils/maintel/__init__.py
+++ python/lsst/ts/fbs/utils/maintel/__init__.py
@@ -20,9 +20,6 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from .basis_functions import *
-from .ddf_presched import *
 from .make_fieldsurvey_scheduler import *
 from .make_scheduler import *
 from .surveys import *
-from .sv_config import *
-from .sv_surveys import *
