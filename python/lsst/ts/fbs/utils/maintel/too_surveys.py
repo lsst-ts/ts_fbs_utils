a233c68aef8c194adac84557ada4c449cb9027e3
Lynne Jones <ljones.uw@gmail.com>
2026-07-28 23:39:20 -0700

    Add v5.3 support

--- python/lsst/ts/fbs/utils/maintel/too_surveys.py
+++ python/lsst/ts/fbs/utils/maintel/too_surveys.py
@@ -32,9 +32,9 @@ from rubin_scheduler.utils import DEFAULT_NSIDE
 
 from .lsst_surveys import (
     EXPTIME,
-    NEXP,
     SCIENCE_PROGRAM,
-    safety_masks,
+    STANDARD_MASK_DEFAULTS,
+    standard_masks,
 )
 
 
@@ -42,11 +42,8 @@ def gen_too_surveys(
     nside: int = DEFAULT_NSIDE,
     detailer_list: list[detailers.BaseDetailer] | None = None,
     too_footprint: npt.NDArray | None = None,
-    split_long: bool = False,
-    long_exp_nsnaps: int = 2,
-    n_snaps: int = NEXP,
     science_program: str = SCIENCE_PROGRAM,
-    safety_mask_params: dict | None = None,
+    standard_mask_params: dict | None = None,
 ) -> list[ToOScriptedSurvey]:
     """Generate a list of ToO surveys to follow up
     events passed in Conditions.
@@ -59,19 +56,9 @@ def gen_too_surveys(
         List of survey detailers.
     too_footprint : `np.ndarray` or None
         Footprint to contain ToOs within (such as the lsst footprint).
-    split_long : `bool`
-        Split long exposures (longer than 30s with the current defaults in
-        rubin_scheduler) into shorter exposures (60s) or not?
-        Splitting long exposures requires creation of nightly coadds, which
-        is not currently available. However long exposures may also pose
-        risks of tripping the sensors.
-    long_exp_nsnaps : `int`
-        The number of snaps for longer exposures. (60s??)
-    n_snaps : `int`
-        The number of snaps per visit for other exposures. (??)
     science_program : `str`
         Metadata to identify the science program for the visit.
-    safety_mask_params : `dict` or None
+    standard_mask_params : `dict` or None
         A dictionary of additional kwargs to mass to the standard safety masks.
 
     Returns
@@ -80,13 +67,13 @@ def gen_too_surveys(
         A list of ToO surveys configured to trigger a pre-specified sequence
         of visits in response to ToO events in the Conditions objects.
     """
-    if safety_mask_params is None:
-        safety_mask_params = {}
-        safety_mask_params["nside"] = nside
+    if standard_mask_params is None:
+        standard_mask_params = STANDARD_MASK_DEFAULTS
+        standard_mask_params["nside"] = nside
     else:
-        safety_mask_params = deepcopy(safety_mask_params)
+        standard_mask_params = deepcopy(standard_mask_params)
     # No value of shadow_minutes with ToO surveys?
-    masks = safety_masks(**safety_mask_params)
+    masks = standard_masks(**standard_mask_params)
 
     too_surveys = []
 
@@ -117,9 +104,7 @@ def gen_too_surveys(
             detailers=deepcopy(detailer_list),
             too_types_to_follow=["GW_case_A"],
             survey_name="ToO, GW_case_A",
-            split_long=split_long,
             flushtime=48.0,
-            n_snaps=long_exp_nsnaps,
             # Update target_name to match the alert event ID
             target_name_base="GW_case_A",
             observation_reason="too_gw_case_a",
@@ -151,9 +136,7 @@ def gen_too_surveys(
             target_name_base="GW_case_B_C",
             observation_reason="too_gw_case_b_c",
             science_program=science_program,
-            split_long=split_long,
             flushtime=48,
-            n_snaps=long_exp_nsnaps,
         )
     )
 
@@ -181,9 +164,7 @@ def gen_too_surveys(
             target_name_base="GW_case_D_E",
             observation_reason="too_gw_case_d_e",
             science_program=science_program,
-            split_long=split_long,
             flushtime=48,
-            n_snaps=long_exp_nsnaps,
             event_gen_detailers=None,
         )
     )
@@ -221,9 +202,7 @@ def gen_too_surveys(
             target_name_base="GW_case_large",
             observation_reason="too_gw_case_large",
             science_program=science_program,
-            split_long=split_long,
             flushtime=48,
-            n_snaps=long_exp_nsnaps,
             event_gen_detailers=None,
         )
     )
@@ -278,9 +257,7 @@ def gen_too_surveys(
             target_name_base="BBH",
             observation_reason="too_bbh",
             science_program=science_program,
-            split_long=split_long,
             flushtime=48,
-            n_snaps=n_snaps,
             event_gen_detailers=event_detailers,
         )
     )
@@ -310,9 +287,7 @@ def gen_too_surveys(
             target_name_base="LensedBNS_A",
             observation_reason="too_lensed_bns_a",
             science_program=science_program,
-            split_long=split_long,
             flushtime=48.0,
-            n_snaps=n_snaps,
         )
     )
 
@@ -338,9 +313,7 @@ def gen_too_surveys(
             target_name_base="LensedBNS_B",
             observation_reason="too_lensed_bns_b",
             science_program=science_program,
-            split_long=split_long,
             flushtime=48.0,
-            n_snaps=long_exp_nsnaps,
         )
     )
 
@@ -378,9 +351,7 @@ def gen_too_surveys(
             target_name_base="neutrino",
             observation_reason="too_neutrino",
             science_program=science_program,
-            split_long=split_long,
             flushtime=20 * 24,
-            n_snaps=n_snaps,
         )
     )
 
@@ -412,9 +383,7 @@ def gen_too_surveys(
             target_name_base="SSO_night",
             observation_reason="too_sso_general",
             science_program=science_program,
-            split_long=split_long,
             flushtime=3.0,
-            n_snaps=n_snaps,
         )
     )
 
@@ -439,9 +408,7 @@ def gen_too_surveys(
             target_name_base="SSO_twi",
             observation_reason="too_sso_twi",
             science_program=science_program,
-            split_long=split_long,
             flushtime=3.0,
-            n_snaps=n_snaps,
         )
     )
 
@@ -473,9 +440,7 @@ def gen_too_surveys(
             target_name_base="SN_Galactic",
             observation_reason="too_sn_galactic",
             science_program=science_program,
-            split_long=split_long,
             flushtime=48.0,
-            n_snaps=n_snaps,
         )
     )
 
